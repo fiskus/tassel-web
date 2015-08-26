4999708049045548fb8cdc43fe26dd5a12f1fe0f
Maxim Chervonny <fiskus@chervonny.ru>
2015-08-27 01:11:54 +0300

    add title to bookmark

--- backend/routes/update.js
+++ backend/routes/update.js
@@ -1,3 +1,8 @@
+var Promise = require('bluebird');
+var http = require('http');
+var https = require('https');
+var cheerio = require('cheerio');
+
 var DB = require('../db/index.js');
 
 function renderJson (res, json) {
@@ -5,15 +10,38 @@ function renderJson (res, json) {
     res.send(json);
 }
 
+function getTitle (url) {
+    return new Promise(function (resolve, reject) {
+        var method = url.indexOf('https://') === 0 ? https : http;
+        method.get(url, function (res) {
+            var content;
+            res.on('error', reject);
+            res.on('data', function (data) {
+                content += data;
+            });
+            res.on('end', function () {
+                var title = cheerio.load(content)('title').text();
+                resolve(title);
+            });
+        });
+    });
+}
+
 function Update (req, res) {
     var url = req.body.url;
-    new DB.Bookmarks({
-        url: url
-    })
-        .save()
+    getTitle(url)
+        .then(function (title) {
+            return new DB.Bookmarks({
+                url: url,
+                title: title,
+                userId: req.session.passport.user
+            }).save();
+        })
         .then(function () {
             renderJson(res, {
-                success: true
+                success: true,
+                session: req.session,
+                isAuthenticated: req.isAuthenticated()
             });
         });
 }
